ba2e982c30e901323f9695fd70099b58d23a8ec4
James Morris <james@jmoz.co.uk>
2012-11-16 20:28:31 +0000

    added url provider and bridge

--- web/index.php
+++ web/index.php
@@ -5,6 +5,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new Silex\Application();
 $app['debug'] = true;
 
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__ . '/../views'
 ));
@@ -13,7 +14,14 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
  * Index
  */
 $app->get('/', function () use ($app) {
-    return $app['twig']->render('index.twig');
-});
+    return $app['twig']->render('index.html.twig');
+})->bind('home');
+
+/**
+ * Facebook
+ */
+$app->get('/facebook', function () use ($app) {
+    return $app['twig']->render('facebook.html.twig');
+})->bind('facebook');
 
 $app->run();
