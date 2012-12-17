7fde8d643e3ed1c11d052d64de304c837e0b7706
James Morris <james@jmoz.co.uk>
2012-12-17 21:52:34 +0000

    echo

--- install.sh
+++ install.sh
@@ -2,9 +2,12 @@
 cd `dirname $0`
 
 if [[ ! -f composer.phar ]]; then
+	echo "*** curl composer and install"
 	curl -s http://getcomposer.org/installer | php
 else
+	echo "*** composer self-update"
 	php composer.phar self-update
 fi
 
+echo "*** composer install"
 php composer.phar -o install
